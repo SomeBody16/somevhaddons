507a5d68229af6382c98fabfcdc330aeceda5d5f
Filip Nowakowski <f.nowakowski2000@gmail.com>
2024-01-20 12:47:43 +0100

    Fixed enchanter

--- src/main/java/network/something/somevhaddons/mixin/EnchantmentEntryMixin.java
+++ src/main/java/network/something/somevhaddons/mixin/EnchantmentEntryMixin.java
@@ -1,12 +1,14 @@
 package network.something.somevhaddons.mixin;
 
 import iskallia.vault.util.EnchantmentEntry;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.enchantment.Enchantment;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(EnchantmentEntry.class)
 public class EnchantmentEntryMixin {
@@ -25,6 +27,21 @@ public class EnchantmentEntryMixin {
         var registryName = this.enchantment.getRegistryName();
         if (registryName != null && registryName.toString().equals("minecraft:fortune")) {
             this.level = 4;
+        }
+    }
+
+    @Inject(
+            method = "isValid()Z",
+            at = @At("RETURN"),
+            cancellable = true,
+            remap = false
+    )
+    private void isValid(CallbackInfoReturnable<Boolean> ci) {
+        ResourceLocation registryName = this.enchantment.getRegistryName();
+        if (registryName != null
+                && registryName.toString().equals("minecraft:fortune")
+                && this.level <= 4) {
+            ci.setReturnValue(true);
         }
     }
 
